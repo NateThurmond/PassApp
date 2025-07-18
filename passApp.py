2c388dff02555237160e0c00f1f1b0c2effead99
NateThurmond <nathanthurmond@yahoo.com>
2025-07-17 22:25:05 -0500

    Added index template file with site icon

--- passApp.py
+++ passApp.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 # Path to your KeePass database on a mounted network share
-KEEPASS_FILE_PATH = r'/Volumes/networkShare/MyVault.kdbx'
+KEEPASS_FILE_PATH = r'/Volumes/Backups.backupdb/passClientsDb.kdbx'
 
 @app.route('/')
 def index():
+    db_exists = 'File not found'
     if os.path.exists(KEEPASS_FILE_PATH):
-        return 'It exists'
-    else:
-        return 'File not found', 404
+        db_exists = 'Pass DB File Found'
+    return render_template('index.html', message=db_exists)
 
 if __name__ == '__main__':
     app.run(debug=True)

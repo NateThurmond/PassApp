b9c63ce8b154a33a99546f8d47d66917ca6b2435
NateThurmond <nathanthurmond@yahoo.com>
2025-07-20 03:51:09 -0500

    Updated backend python script with cookie setting/parsing for DB file path

--- passApp.py
+++ passApp.py
@@ -1,21 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, make_response
 from pykeepass import PyKeePass
 import os
 
 app = Flask(__name__)
 
-# Path to your KeePass database on a mounted network share
-KEEPASS_FILE_PATH = r'/Volumes/Backups.backupdb/passClientsDb.kdbx'
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    db_exists = 'File not found'
+    KEEPASS_FILE_PATH = ''
+    message = 'File not found'
     entries = []
+
+    if request.method == 'POST':
+        KEEPASS_FILE_PATH = request.form.get('keepass_path')
+    else:
+        existingCookie = request.cookies.get('keepass_path')
+        if existingCookie:
+            KEEPASS_FILE_PATH = existingCookie
+
     if os.path.exists(KEEPASS_FILE_PATH):
-        db_exists = 'Pass DB File Found'
+        message = 'Pass DB File Found'
         kp = PyKeePass(KEEPASS_FILE_PATH, password='xxxxxx')
         entries = kp.entries
-    return render_template('index.html', message=db_exists, entries=entries)
+
+    renderVars = dict(
+        message=message,
+        entries=entries,
+        KEEPASS_FILE_PATH=KEEPASS_FILE_PATH
+    )
+
+    response = make_response(render_template('index.html', **renderVars))
+    if request.method == 'POST':
+        response.set_cookie('keepass_path', KEEPASS_FILE_PATH, max_age=60*60*24*365)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
